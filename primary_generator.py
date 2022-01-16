fc74c93997b7df7f25002ac79575414f21dec21e
mako3577 <61459721+mako3577@users.noreply.github.com>
2022-01-16 22:04:35 +0100

    Add files via upload

---
+++ primary_generator.py
@@ -0,0 +1,31 @@
+import time
+
+
+def generator(limit):
+    """ Generator is creating and printing primary numbers.
+
+    Parameters:
+        limit (int): Function breaks when limit is reched.
+    """
+    start = time.process_time()
+    liczba = 1
+    count = 1
+    print("2 is a primary number")
+
+    while liczba < limit:
+        suma_dzielnikow = 0                   # reset liczby dzielnikow
+        liczba += 2
+        for dzielnik in range(3, liczba + 1, 2):
+            if liczba % dzielnik == 0:
+                suma_dzielnikow += 1
+                if suma_dzielnikow > 1:       # przestaje szukac kolejnych
+                    break                     # dzielnikow po osiagnieciu 2
+                if dzielnik == liczba:
+                    if suma_dzielnikow == 1:
+                        print(liczba, "to liczba pierwsza")
+                        count += 1
+    finish = time.process_time()
+    print(f'There are {count} primal numbers in range {limit}.\nProcess took {finish - start}s')
+
+
+generator(20000)
